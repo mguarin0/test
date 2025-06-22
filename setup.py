80e864fa709c48e9dae8bee4a4aedbd20a5c943c
mguarin0 <33071894+mguarin0@users.noreply.github.com>
2025-06-22 17:40:45 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,39 +1,37 @@
-from setuptools import setup
-from setuptools import Extension
+from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
-import pybind11
 import os
-import sys
 import subprocess
 
 class CMakeExtension(Extension):
-    def __init__(self, name, sourcedir="."):
+    def __init__(self, name):
         super().__init__(name, sources=[])
-        self.sourcedir = os.path.abspath(sourcedir)
 
 class CMakeBuild(build_ext):
     def run(self):
         for ext in self.extensions:
             self.build_cmake(ext)
-    
+
     def build_cmake(self, ext):
-        build_dir = os.path.abspath(self.build_temp)
-        os.makedirs(build_dir, exist_ok=True)
+        build_temp = os.path.abspath(self.build_temp)
+        os.makedirs(build_temp, exist_ok=True)
+        ext_dir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
 
-        cfg = "Debug" if self.debug else "Release"
-        cmake_args = [
-            f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={os.path.abspath(self.get_ext_fullpath(ext.name))}",
-            f"-DCMAKE_BUILD_TYPE={cfg}",
-        ]
-
-        build_args = ["--config", cfg]
-        subprocess.check_call(["cmake", ext.sourcedir] + cmake_args, cwd=build_dir)
-        subprocess.check_call(["cmake", "--build", "."] + build_args, cwd=build_dir)
+        subprocess.check_call([
+            "cmake",
+            "-S", "cpp",
+            "-B", build_temp,
+            f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={ext_dir}",
+            "-DCMAKE_BUILD_TYPE=Release",
+        ])
+        subprocess.check_call(["cmake", "--build", build_temp])
 
 setup(
     name="vector_add",
     version="0.1.0",
-    ext_modules=[CMakeExtension("vector_add")],
+    packages=["vector_add"],
+    package_dir={"": "src"},
+    ext_modules=[CMakeExtension("vector_add._vector_add")],  # creates vector_add/_vector_add.so
     cmdclass={"build_ext": CMakeBuild},
     zip_safe=False,
 )
